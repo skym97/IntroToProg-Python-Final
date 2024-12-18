b11bba6e8f52f27e907e0bbecf70d68aacb48be2
Andy Sul <88988892+skym97@users.noreply.github.com>
2024-12-17 21:41:57 -0800

    Update processing_classes.py
    removed comment headings

--- employee-ratings/src/processing_classes.py
+++ employee-ratings/src/processing_classes.py
@@ -1,10 +1,3 @@
-# ------------------------------------------------------------------------------------------------- #
-# Title: Assignment08-Starter files
-# # Description: A collection of classes for managing the application
-# ChangeLog: (Who, When, What)
-# RRoot,1.5.2030,Created Script
-# Andy Sul, 11/30/24, Modified Script
-# ------------------------------------------------------------------------------------------------- #
 
 from data_classes import Employee
 import json
